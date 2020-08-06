31de33ecb36de9f7c52c885023aace1ac7753e9d
Vinicius Hein <vinicius.hein@gmail.com>
2020-08-06 06:46:49 -0300

    front-end concluded

---
+++ scripts/addField.js
@@ -0,0 +1,18 @@
+document.querySelector('#add-time')
+.addEventListener("click", cloneField)
+
+function cloneField() {
+    // duplica os campos
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+
+    // limpar os campos
+    const fields = newFieldContainer.querySelectorAll('input')
+
+    fields.forEach(function (field) {
+        // pega o field do momento e limpa
+        field.value = ''
+    })
+
+    // diz onde será colocado
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+}
