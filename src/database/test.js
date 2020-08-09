2741a35c8e74fa151b8c08106c7d7ce1a95d9eeb
Vinicius Hein <vinicius.hein@gmail.com>
2020-08-09 01:12:04 -0300

    completed database

---
+++ src/database/test.js
@@ -0,0 +1,62 @@
+const Database = require('./db')
+const createProffy = require('./createProffy')
+
+Database.then(async (db) => {
+    proffyValue = {
+        name: "Diego Fernandes", 
+        avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "950464324",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+    }
+
+    classValue = {
+        subject: 1,
+        cost: "20",
+        // o proffy id virá pelo banco de dados
+    }
+
+    classScheduleValues = [
+        // class_id virá pelo banco de dados, após cadastrarmos o class
+        {
+            weekday: 1,
+            time_from: 720,
+            time_to: 1200
+        },
+        {
+            weekday: 1,
+            time_from: 720,
+            time_to: 1200
+        },
+    ]
+
+    // await createProffy(db, {proffyValue, classValue, classScheduleValues})
+
+    // Consultar os dados inseridos
+
+    // todos os proffys
+    const selectedProffys =  await db.all("SELECT * FROM proffys")
+    // console.log(selectedProffys)
+
+    // consultar as classes de um determinado professor
+    // e trazer junto os dados do professor
+    const selectClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1;
+    `)
+    // console.log(selectClassesAndProffys)
+
+    // o horário que a pessoa trabalha, por exemplo, é das 8h - 18h
+    // o horário do time_from (8h) precisa ser menor ou igual ao horário solicitado
+    //  o time_to precisa ser acima
+    const selectClassesSchedules = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = "1"
+        AND class_schedule.weekday = "1"
+        AND class_schedule.time_from <= "720"
+        AND class_schedule.time_to > "1720"
+    `)
+    console.log(selectClassesSchedules)
+})
--- src/server.js
+++ src/server.js
@@ -1,95 +1,16 @@
-// Dados
-const proffys = [
-    {
-        name: "Diego Fernandes", 
-        avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "950464324",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost: "20",
-        weekday: [0],
-        time_from: [720],
-        time_to: [1200]
-    },
-    {
-        name: "Diego Fernandes", 
-        avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "950464324",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost: "20",
-        weekday: [0],
-        time_from: [720],
-        time_to: [1200]
-    }
-]
-const subjects = [
-    "Artes",
-    "Biologia",
-    "Ciências",
-    "Educação física",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Português",
-    "Química",
-]
-
-const weekdays = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado"
-]
-
-
-// Funcionalidades
-
-function getSubject(subjectNumber) {
-    const position = + subjectNumber - 1
-    return subjects[position]
-}
-
-function pageLanding(req,res) {
-    return res.render("index.html")
-}
-
-function pageStudy(req,res) {
-    const filters = req.query
-
-    return res.render("study.html", { proffys, filters, subjects, weekdays })
-}
-
-function pageGiveClasses(req,res) {
-    const data = req.query
-
-    const isNotEmpty = Object.keys(data).length > 0
-
-    // adicionar data aos proffys
-    if (isNotEmpty) {
-
-        data.subject = getSubject(data.subject)
-
-        proffys.push(data)
-
-        return res.redirect("/study")
-    }
-
-
-    return res.render("give-classes.html", { subjects, weekdays })
-}
-
-
 // Servidor
 const express = require('express')
 const server = express()
-const nunjucks = require('nunjucks')
+
+const { 
+    pageLanding, 
+    pageStudy, 
+    pageGiveClasses, 
+    saveClasses
+ } = require('./pages')
 
 // configurando nunjucks (template engine)
+const nunjucks = require('nunjucks')
 nunjucks.configure('src/views', {
     express: server,
     noCache: true,
@@ -97,10 +18,14 @@ nunjucks.configure('src/views', {
 
 // Inicio e configuração do servidor
 server
+// receber os dados do req.body
+.use(express.urlencoded({ extended: true }))
 // configurar arquivos estáticos (css, scripts, imagens)
 .use(express.static("public"))
 // rotas da aplicação
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
+
 .listen(5500)
